3ea285d240f9f1d2e9dc15492881934c790cca3c
Pgreeni <Phillip.Greenfield@essexacl.ac.uk>
2026-07-07 20:14:43 +0100

    Phil V2 Staff Attendance Tracker
    Cleaned up code from previous version. Added nested dictionary and sample data within

--- Staff Attendance Tracker.py
+++ Staff Attendance Tracker.py
@@ -1,20 +1,39 @@
 #functions
 
 #nested dictionary
+date = {
+    "2026:07:14": {
+        "employee":["Ryan", "Phil", "Daniela"] , 
+        "attended":["Yes", "Yes", "Yes"] , 
+        "Comment":["", "", ""]},
+    "2026:07:15": {
+        "employee":["Ryan", "Phil", "Daniela"] , 
+        "attended":["No", "Yes", "No"] , 
+        "Comment":["Family Emergency", "", "Sick"]},
+    "2026:07:16": {
+        "employee":["Ryan", "Phil", "Daniela"] , 
+        "attended":["Yes", "No", "Yes"] , 
+        "Comment":["", "Holiday", ""]}
+}
 
 #menu and main loop
 while True:
+    #print(date["2026:07:14"]["employee"][0]) #prints Ryan
+    #print(date["2026:07:14"]["employee"][2]) #prints Daniela
+    #print(date["2026:07:14"]["employee"].index("Phil")) #prints 1
     print("\nThis is what you can do with your Attendance Tracker:\n")
-    print("1 - Add an Employee to your Attendance Tracker")
-    print("2 - Remove an item from your shopping list")
-    print("0 - Exit the shopping list app\n")
+    print("1 - Add a date to your Attendance Tracker")
+    print("2 - Remove a date from your Attendance Tracker")
+    print("3 - Add an employee to a date")
+    print("4 - Remove an employee from a date")
+    print("0 - Exit the Attendance Tracker program\n")
     answer = input("What would you like to do? ")
     if answer == "1":
         print("Add selected")
     elif answer == "2":
         print("Delete selected")
     elif answer == "0":
-        print("App exited")
+        print("Program exited")
         exit()
     else:
         print("Unkown command. Please try again")
